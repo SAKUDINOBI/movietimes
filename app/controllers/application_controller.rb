e57e4b6a91c74213b2fac8336e665af9757c40a8
SIMON AKUDINOBI <s.akudinobi.19@abdn.ac.uk>
2020-12-07 17:32:15 +0000

    commiting search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+include SessionsHelper
+
+def redirect_if_not_found
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_06_164854) do
+ActiveRecord::Schema.define(version: 2020_12_07_163653) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "street_num"
@@ -61,6 +61,15 @@ ActiveRecord::Schema.define(version: 2020_12_06_164854) do
     t.integer "screens_allowed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "addresses", "customers"
